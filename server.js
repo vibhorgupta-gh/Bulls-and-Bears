52801773cf2add93c529cc354b7023a8edefe1cc
VibhorCodecianGupta <VibhorCodecianGupta@users.noreply.github.com>
2018-07-10 12:07:48 +0530

    server

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
-var express    = require("express");
- 	mongoose   = require("mongoose");
- 	bodyParser = require("body-parser");
- 	app        =  express();
+const express = require('express');
+const	mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const port = process.env.PORT || 3000;
+const ip = process.env.IP || '127.0.0.1';
+const config = require('./config.js');
+const app = express();
 
+app.use(cookieParser());
 
+//middleware for POST data
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(process.env.PORT , process.env.IP , function(){
-	console.log("SERVER HAS STARTED");
+app.get('/', (req, res)=> {
+  res.send('We are now live!')
+})
+
+app.listen(port, ip, function(){
+	console.log('Magic happens at ' + config.SERVER_URL + '!');
 })
