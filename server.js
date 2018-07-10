335ce2a32852ea2448a07189344261f7ea594a91
nitin dhemiwal <dhemiwal.nitin@gmail.com>
2018-07-10 10:47:08 +0530

    changing port to 3000

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
-var express    = require("express");
- 	mongoose   = require("mongoose");
- 	bodyParser = require("body-parser");
- 	app        =  express();
+const express    = require("express");
+const mongoose   = require("mongoose");
+const bodyParser = require("body-parser");
+const app        = express();
+var port       = process.env.PORT||3000;
+var ip         = process.env.IP||'localhost';
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(process.env.PORT , process.env.IP , function(){
-	console.log("SERVER HAS STARTED");
-})
+
+
+app.listen(port , ip , function(){
+	console.log("SERVER HAS STARTED \nIP address : ",ip ,"\nlistening on Port : ",port);
+});
