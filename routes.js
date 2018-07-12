137e9ee5d21a2589c58c77df9f6b7e9d8bce183d
Pratyushx <sharmapratyush_bt2k16@dtu.ac.in>
2018-07-13 00:31:35 +0530

    routes updated

--- routes.js
+++ routes.js
@@ -14,4 +14,74 @@ router.delete('/users',(req,res,next)=>{
         res.json(user);
     })
 })
-module.exports = router;
+
+      
+ //GET 
+
+ router.get('/leaderboard',(req,res,next)=>{
+    user.find({},(err,user)=>{
+        res.send("foobar");
+    });
+  });
+
+  router.get('/companylist',(req,res,next)=>{
+    user.find({},(err,company)=>{
+        res.send("foobar");
+    });
+  });
+
+  router.get('/costumerdetail',(req,res,next)=>{
+    user.find({},(err,company)=>{
+        res.send("foobar");
+    });
+  });
+
+  router.get('/companydetail',(req,res,next)=>{
+    user.find({},(err,company)=>{
+        res.send("foobar");
+    });  
+  });
+
+  router.get('/newslist',(req,res,next)=>{
+    user.find({},(err,news)=>{
+        res.send("foobar");
+    });
+  });
+
+  router.post('/buy',(req,res,next)=>{
+    user.find({},(err,company)=>{
+        res.send("foobar");
+    });
+  });
+
+  router.post('/sell',(req,res,next)=>{
+    user.find({},(err,company)=>{
+        res.send("foobar");
+    });
+  });
+
+  router.post('/short',(req,res,next)=>{
+    user.find({},(err,company)=>{
+        res.send("foobar");
+    });
+  });
+
+  router.post('/cover',(req,res,next)=>{
+    user.find({},(err,company)=>{
+        res.send("foobar");
+    });
+  });
+
+  router.post('/takeloan',(req,res,next)=>{
+    user.find({},(err,company)=>{
+        res.send("foobar");
+    });
+  });
+
+  router.post('/repayloan',(req,res,next)=>{
+    user.find({},(err,company)=>{
+        res.send("foobar");
+    });
+  });
+
+  module.exports = router;
