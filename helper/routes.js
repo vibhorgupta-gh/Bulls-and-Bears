c55a2ad786215752f629815f4600bc3699899293
Sparsh Katiyar <sparshkat99@gmail.com>
2018-08-21 23:44:23 +0530

    a helper folder created and rerfactored the code by a lot

---
+++ helper/routes.js
@@ -0,0 +1,15 @@
+// the schema one would be required
+
+exports.editCompany = function(req,res){
+  res.send("foobar");
+}
+
+exports.deleteCompany = function(req,res){
+  res.send("foobar");
+}
+
+exports.addCompany = function(req,res){
+  res.send("foobar");
+}
+
+module.exports = exports ;
--- routes/admin.js
+++ routes/admin.js
@@ -2,7 +2,7 @@ const express = require('express');// pass passport for configuration
 const router = express.Router();
 const { isLoggedIn, isAdmin } = require('../utils/middleware') ;
 const user = require('../model/user');
-
+const helpers = require('../helpers/routes.js')
 
 
 // router.delete('/admin/users',(req,res,next)=>{
@@ -11,23 +11,29 @@ const user = require('../model/user');
 //     })
 // });
 
-router.get("admin/companies", (req,res)=> {
-	res.send("foobar");
-});
-
-router.post("/admin/add_company" , (req,res) => {
-	res.send("foobar");
-});
-
-router.post("/admin/delete_company" , (req,res) => {
-	res.send("foobar");
-});
+router.route('/admin/company')
+	.post(helpers.editCompany)
+	.delete(helpers.deleteCompany)
+	.post(helpers.addCompany);
 
 
-router.post("/admin/edit_news" , (req,res) => {
-	res.send("foobar");
-});
-
-router.post("/admin/edit_company" , (req,res) => {
-	res.send("foobar");
-});
+// router.get("admin/companies", (req,res)=> {
+// 	res.send("foobar");
+// });
+//
+// router.post("/admin/add_company" , (req,res) => {
+// 	res.send("foobar");
+// });
+//
+// router.post("/admin/delete_company" , (req,res) => {
+// 	res.send("foobar");
+// });
+//
+//
+// router.post("/admin/edit_news" , (req,res) => {
+// 	res.send("foobar");
+// });
+//
+// router.post("/admin/edit_company" , (req,res) => {
+// 	res.send("foobar");
+// });
