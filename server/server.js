4ae3c6fc09850c78fb8851d1ae256552127f5589
VibhorCodecianGupta <guptavibhu98@gmail.com>
2018-10-28 15:48:55 +0530

    react setup and file structure overhaul

---
+++ server/server.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const port = process.env.PORT || 8080;
+const passport = require('passport');
+const config = require('./config.js');
+const app = express();
+
+app.use(session({
+  secret: 'bnbisgoodbnbisgood',
+  resave: true,
+  saveUninitialized: true
+}));
+// session secret
+app.use(passport.initialize());
+app.use(passport.session());
+require('./auth/social')(app, passport);
+
+app.use(cookieParser());
+// persistent login sessions
+//middleware for POST data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+//connect to mongoose db
+mongoose.connect(config.SECRETS.database.url, {
+  useNewUrlParser: true
+});
+//on connected
+mongoose.connection.on('connected', () => {
+  console.log('connected to database :)');
+});
+//on error
+mongoose.connection.on('error', (err) => {
+  if (err) {
+    console.log('error is ' + err + config.SECRETS.database.url);
+  }
+});
+
+require('./routes/routes')(app, passport)
+require('./routes/admin')(app, passport)
+
+app.get('/', (req, res) => {
+  res.send('We are now live!')
+})
+
+app.get('*', (req, res) => {
+  res.send('Out of Bounds!')
+})
+
+app.listen(port, function () {
+  console.log('Magic happens at port ' + port + '!');
+})
+
+
+module.exports = app;
