6467203a7c8584758577a862cf2deb1dc85b0329
Zephyr007 <dhemiwal.nitin@gmail.com>
2019-01-14 16:24:28 +0530

    resolving merge conflicts

--- server/config.js
+++ server/config.js
@@ -6,13 +6,10 @@ switch (config.DEPLOY) {
 
   case 'localhost':
   default:
-<<<<<<< HEAD:config.js
-    config.SERVER_URL = 'http://localhost:3000' ,
-    config.SECRETS = require('./secrets.json')
-=======
+
     config.SERVER_URL = 'http://localhost:8080',
     config.SECRETS = require('./secrets-sample.json')
->>>>>>> 9a847e4f061e48c9cc86c8a73240e55ba1e814aa:server/config.js
+
     break
 }
 
