7d6900775ffd05e1ed474a65ff4db99bca8de2b7
Zephyr007 <dhemiwal.nitin@gmail.com>
2019-01-31 02:18:50 +0530

    drop-down menu

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -36,114 +36,119 @@ class NavBar extends Component {
 
   render() {
     return (
-      <div class="mainheader-area">
-        <div class="container">
-          <div class="row align-items-center">
-            <div class="col-md-1">
-              <div class="logo">
-                <a href="/d">
-                  <img src="assets/images/icon/bnb.png" alt="logo" />
+        <div className="mainheader-area">
+            <div className="container">
+                <div className="row align-items-center">
+                    <div className="col-md-1">
+                        <div className="logo">
+                            <a href="/d">
+                                <img src="assets/images/icon/bnb.png" alt="logo"/>
 
-                </a>
+                            </a>
 
-                
-              </div>
-              {/* <p>Bulls And Bears</p> */}
-            </div>
-            <div class="col-md-4">
-              <a href="/dashboard" style={{align:"left"}}>
-                <img src="assets/images/icon/Bulls N Bears.png" alt="logo" />
 
-              </a>
-            </div>
-            <div class="col-md-7 clearfix text-right">
-              <div class="clearfix d-md-inline-block d-block">
-                <div class="user-profile m-0">
-                  <img
-                    class="avatar user-thumb"
-                    src={`https://graph.facebook.com/${
-                      this.state.image
-                      }/picture`}
-                    alt="avatar"
-                  />
-                  <h4 class="user-name dropdown-toggle" data-toggle="dropdown">
-                    {this.state.name} <i class="fa fa-angle-down" />
-                  </h4>
+                        </div>
+                    </div>
+                    <div className="col-md-4">
+                        <a href="/dashboard" style={{align: "left"}}>
+                            <img src="assets/images/icon/Bulls N Bears.png" alt="logo"/>
 
+                        </a>
+                    </div>
+                    <div className="col-md-7 clearfix text-right">
+                        <div className="clearfix d-md-inline-block d-block">
+                            <div className="user-profile m-0">
+                                <img
+                                    className="avatar user-thumb"
+                                    src={`https://graph.facebook.com/${
+                                        this.state.image
+                                        }/picture`}
+                                    alt="avatar"
+                                />
+                                <h4 className="user-name dropdown-toggle" data-toggle="dropdown">
+                                    {this.state.name} <i className="fa fa-angle-down"> </i>
+                                </h4>
+
+                                <div className="dropdown-menu">
+                                    <a className="dropdown-item" href="#">Message</a>
+                                    <a className="dropdown-item" href="/logout">Log Out</a>
+                                </div>
+
+
+                            </div>
+                        </div>
+                    </div>
                 </div>
-              </div>
             </div>
-          </div>
+            <div className="header-area header-bottom">
+                <div className="container">
+                    <div className="row align-items-center">
+                        <div className="col-lg-9  d-none d-lg-block">
+                            <div className="horizontal-menu">
+                                <nav>
+                                    <ul id="nav_menu">
+                                        <li>
+                                            <Link to={"/dashboard"}>
+                                                <i className="ti-dashboard"/>
+                                                <span>Dashboard</span>
+                                            </Link>
+                                        </li>
+                                        <li>
+                                            <Link to={"/market"}>
+                                                <i className="ti-dashboard"/>
+                                                <span>market</span>
+                                            </Link>
+                                        </li>
+                                        <li>
+                                            <Link to={"/leaderboard"}>
+                                                <i className="ti-dashboard"/>
+                                                <span>leaderboard</span>
+                                            </Link>
+                                        </li>
+                                        <li>
+                                            <Link to={"/news"}>
+                                                <i className="ti-dashboard"/>
+                                                <span>News</span>
+                                            </Link>
+                                        </li>
+                                    </ul>
+                                </nav>
+                            </div>
+                        </div>
+                        <div className="col-12 d-block d-lg-none">
+                            <div id="mobile_menu">
+                                <ul id="nav_menu">
+                                    <li>
+                                        <Link to={"/dashboard"}>
+                                            <i className="ti-dashboard"/>
+                                            <span>Dashboard</span>
+                                        </Link>
+                                    </li>
+                                    <li>
+                                        <Link to={"/market"}>
+                                            <i className="ti-dashboard"/>
+                                            <span>market</span>
+                                        </Link>
+                                    </li>
+                                    <li>
+                                        <Link to={"/leaderboard"}>
+                                            <i className="ti-dashboard"/>
+                                            <span>leaderboard</span>
+                                        </Link>
+                                    </li>
+                                    <li>
+                                        <Link to={"/news"}>
+                                            <i className="ti-dashboard"/>
+                                            <span>News</span>
+                                        </Link>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="header-area header-bottom">
-          <div class="container">
-            <div class="row align-items-center">
-              <div class="col-lg-9  d-none d-lg-block">
-                <div class="horizontal-menu">
-                  <nav>
-                    <ul id="nav_menu">
-                      <li>
-                        <Link to={"/dashboard"}>
-                          <i class="ti-dashboard" />
-                          <span>Dashboard</span>
-                        </Link>
-                      </li>
-                      <li>
-                        <Link to={"/market"}>
-                          <i class="ti-dashboard" />
-                          <span>market</span>
-                        </Link>
-                      </li>
-                      <li>
-                        <Link to={"/leaderboard"}>
-                          <i class="ti-dashboard" />
-                          <span>leaderboard</span>
-                        </Link>
-                      </li>
-                      <li>
-                        <Link to={"/news"}>
-                          <i class="ti-dashboard" />
-                          <span>News</span>
-                        </Link>
-                      </li>
-                    </ul>
-                  </nav>
-                </div>
-              </div>
-              <div class="col-12 d-block d-lg-none">
-                <div id="mobile_menu" >
-                  <ul id="nav_menu">
-                    <li>
-                      <Link to={"/dashboard"}>
-                        <i class="ti-dashboard" />
-                        <span>Dashboard</span>
-                      </Link>
-                    </li>
-                    <li>
-                      <Link to={"/market"}>
-                        <i class="ti-dashboard" />
-                        <span>market</span>
-                      </Link>
-                    </li>
-                    <li>
-                      <Link to={"/leaderboard"}>
-                        <i class="ti-dashboard" />
-                        <span>leaderboard</span>
-                      </Link>
-                    </li>
-                    <li>
-                      <Link to={"/news"}>
-                        <i class="ti-dashboard" />
-                        <span>News</span>
-                      </Link>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
     );
   }
 }
