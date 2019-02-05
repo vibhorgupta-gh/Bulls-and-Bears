983411b51d7b9b4d0ab2208c1eac28872027066a
shivanggarg1998 <shivanggarg1998@gmail.com>
2019-02-06 00:51:37 +0530

    deploy

--- server/auth/social.js
+++ server/auth/social.js
@@ -171,7 +171,7 @@ module.exports = function (app, passport) {
 
     // handle the callback after facebook has authenticated the user
     app.get('/auth/facebook/callback', passport.authenticate('facebook', {
-        successRedirect: '/landing',
+        successRedirect: '/dashboard',
         failureRedirect: '/',
         failureFlash: true
     }));
@@ -189,7 +189,7 @@ module.exports = function (app, passport) {
     // the callback after google has authenticated the user
     app.get('/auth/google/callback',
         passport.authenticate('google', {
-            successRedirect: '/landing',
+            successRedirect: '/dashboard',
             failureRedirect: '/'
         }));
     // route for logging out
