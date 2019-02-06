f0bca624d574f5ece91d8e466194e07cdaa52fe9
VibhorCodecianGupta <guptavibhu98@gmail.com>
2019-02-06 22:15:16 +0530

    changes

---
+++ client/build/precache-manifest.69935519a9ec2fed0ed543127232063d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3065ed59aad7c392f944",
+    "url": "/static/js/main.3065ed59.chunk.js"
+  },
+  {
+    "revision": "8b2af1fa30f7f2680194",
+    "url": "/static/js/1.8b2af1fa.chunk.js"
+  },
+  {
+    "revision": "8b2af1fa30f7f2680194",
+    "url": "/static/css/1.f7841b63.chunk.css"
+  },
+  {
+    "revision": "97976ded48036bc7459a7893211c33b5",
+    "url": "/index.html"
+  }
+];
